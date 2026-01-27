b7f8d3ff4bd44fc5e43515d17b41d8b52e9b9be0
Michael DeCuire <michaeldecuire18@gmail.com>
2026-01-26 19:39:06 -0600

    Vite.config update with routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'fs'
+import { join } from 'path'
 
-// https://vitejs.dev/config/
+// Plugin to create 404.html for GitHub Pages SPA routing
+const create404Plugin = () => {
+  return {
+    name: 'create-404',
+    closeBundle() {
+      const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Redirecting...</title>
+    <script>
+      var pathSegmentsToKeep = 1;
+      var l = window.location;
+      l.replace(
+        l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') +
+        l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/' +
+        l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~') +
+        (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+        l.hash
+      );
+    </script>
+  </head>
+  <body>
+  </body>
+</html>`
+      writeFileSync(join(__dirname, 'dist', '404.html'), html)
+    }
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), create404Plugin()],
   base: '/personal_website/',
 })
